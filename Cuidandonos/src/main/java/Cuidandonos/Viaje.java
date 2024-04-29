739d1ed739e7368c22b31234a5efea8aab77b66c
Juan Francisco Meza Longa <jmezalonga@frba.utn.edu.ar>
2024-04-29 18:13:52 -0300

    Update Viaje.java

--- Cuidandonos/src/main/java/Cuidandonos/Viaje.java
+++ Cuidandonos/src/main/java/Cuidandonos/Viaje.java
@@ -10,10 +10,10 @@ public class Viaje {
     public double calcularTiempoViajeTotal(){
         double demoras = this.calcularDemora();
         double distanciaTotal = calculador.calcularDistanciaViajeTotal(direccionOrigen,paradas);
-        return (distanciaTotal / 72) + demoras;   //72 metros x min es la velocidad promedio de una persona caminando, consideramos "distanciaTotal/72" como la demora de las paradas que no tienen una demora especificada (tiempo que se tarda en llegar de una a la otra)
+        return (distanciaTotal / 72) + demoras; // 72 metros x min es la velocidad promedio de una persona caminando, consideramos "distanciaTotal/72" como la demora de las paradas que no tienen una demora especificada (tiempo que se tarda en llegar de una a la otra)
     }
     private double calcularDemora(){
-        return 0 + paradas.stream().filter(parada->parada.tieneDemora())
+        return paradas.stream().filter(parada->parada.tieneDemora())
                 .mapToDouble(parada->parada.getDemora())
                 .sum();
     }
